d768db338ab5b461c913facb930c97c2701f4b49
sherry-debug715 <bambooyu715@gmail.com>
2021-10-22 10:50:28 -0400

    deploy on heroku

--- react-app/src/components/BusinessesDisplay/index.js
+++ react-app/src/components/BusinessesDisplay/index.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 import { NavLink } from 'react-router-dom';
 import { getAllBusinessesThunk } from '../../store/businesses';
 import Card from '../PictureCard';
-
+import './businessesDisplay.css';
 
 const BusinessesDisplay = () => {
 
