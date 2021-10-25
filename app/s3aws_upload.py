2d5d9459850b9a868ecf52ab446a1248a7bba03c
sherry-debug715 <bambooyu715@gmail.com>
2021-10-25 17:26:08 -0400

    heroku working

--- app/s3aws_upload.py
+++ app/s3aws_upload.py
@@ -3,6 +3,23 @@
 # import os
 # import uuid
 
+#        // "boto3": {
+        # //     "hashes": [
+        # //         "sha256:49ca02fbe8bd74da8f5bba5ae83788ce60354c6628773fa4affff1b58351fc0c",
+        # //         "sha256:a50797af662cc8bec77cda51e92fe0d26f4909780b3dd3430d22c96748c48706"
+        # //     ],
+        # //     "index": "pypi",
+        # //     "version": "==1.19.1"
+        # // },
+        # // "botocore": {
+        # //     "hashes": [
+        # //         "sha256:0a52801ce5a8835bedfc71a3e1143545e17cc146638d4dd23672055738cac253",
+        # //         "sha256:32897b949415873534bd84c95d82485a5f3ce88fc72638bfd829afa4f97be8d6"
+        # //     ],
+        # //     "markers": "python_version >= '3.6'",
+        # //     "version": "==1.22.1"
+        # // },
+
 # BUCKET_NAME = os.environ.get("S3_BUCKET")
 # S3_LOCATION = f"https://{BUCKET_NAME}.s3.amazonaws.com/"
 # ALLOWED_EXTENSIONS = {"png", "jpg", "jpeg", "gif", "mp4", "mov", "wmv"}
--- react-app/src/components/CategoryFilter/index.js
+++ react-app/src/components/CategoryFilter/index.js
@@ -1,45 +1,41 @@
-import { useSelector, useDispatch } from 'react-redux';
-import { useEffect } from 'react';
-import { NavLink, useParams } from 'react-router-dom';
-import { getAllBusinessesThunk } from '../../store/businesses';
-import Card from '../PictureCard';
+// import { useSelector, useDispatch } from 'react-redux';
+// import { useEffect } from 'react';
+// import { NavLink, useParams } from 'react-router-dom';
+// import { getAllBusinessesThunk } from '../../store/businesses';
+// import Card from '../PictureCard';
 
 
-const BusinessOfCategory = () => {
+// const BusinessOfCategory = () => {
 
-    const dispatch = useDispatch();
-    const { categoryId } = useParams()
-    const businessesObj = useSelector(state => state?.businesses)
-    const businesesArray = Object.values(businessesObj)
+//     const dispatch = useDispatch();
+//     const { categoryId } = useParams()
+//     const businessesObj = useSelector(state => state?.businesses)
+//     const businesesArray = Object.values(businessesObj)
 
-    useEffect(() => {
-        dispatch(getAllBusinessesThunk())
-    }, [dispatch]);
+//     useEffect(() => {
+//         dispatch(getAllBusinessesThunk())
+//     }, [dispatch]);
 
-    return (
-        <>
-            <div className="business-of-category-container">
-                { businesesArray?.map(business => {
-                    return (
-                        +business?.category_id === +business?.categoryId ? (
-                            <>
-                                <div className="business-img-container">
-                                    <NavLink style={{ textDecoration:'none'}} to={`/businesses/${business?.id}`}>
-                                        <Card
-                                            src={business?.media_1}
-                                            alt={business?.description}
-                                            businessName={business?.title}
-                                        />
-                                    </NavLink>
-                                </div>
-                            </>
-                        )
-                    )
-                })}
-            </div>
-        </>
-    )
+//     return (
+//         <>
+//             <div className="business-of-category-container">
+//                 { businesesArray?.map(business => (
 
-}
+//                         {+business?.category_id === +business?.categoryId ? (
+//                                 <NavLink key={business?.id} style={{ textDecoration:'none'}} to={`/businesses/${business?.id}`}>
+//                                     <Card
+//                                         src={business?.media_1}
+//                                         alt={business?.description}
+//                                         businessName={business?.title}
+//                                     />
+//                                 </NavLink>
+//                         )
+//                     }
+//                 )}
+//             </div>
+//         </>
+//     )
 
-export default BusinessOfCategory
+// }
+
+// export default BusinessOfCategory
