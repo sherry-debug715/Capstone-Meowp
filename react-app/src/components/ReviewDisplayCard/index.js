678dca61c53028fa5077e22eefc653a9a720a027
sherry-debug715 <bambooyu715@gmail.com>
2021-10-26 13:11:51 -0400

    comments displaying

---
+++ react-app/src/components/ReviewDisplayCard/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import './ReviewDisplayCard.css'
+
+
+function ReviewDisplayCard({userName, userSrc, userAlt, userCity, userState, reviewContent, rating}) {
+
+    return (
+        <>
+            <div className="review-card-container">
+                <div className="user-info-container">
+                    <div className="user-profile-img-container">
+                        <img className="user-profile-img" src={userSrc} alt={userAlt}/>
+                    </div>
+                    <div className="user-info">
+                        <div>{userName}</div>
+                        <div>{userCity} {userState}</div>
+                    </div>
+                </div>
+
+                <div className="review-rating">
+                    {rating}
+                </div>
+
+                <div className="review-content">
+                    {reviewContent}
+                </div>
+            </div>
+
+        </>
+    )
+}
+
+export default ReviewDisplayCard
