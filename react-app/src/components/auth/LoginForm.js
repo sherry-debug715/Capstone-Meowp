7417db73aba632000c6da9bae331f9db7a57f361
sherry-debug715 <bambooyu715@gmail.com>
2021-10-28 10:25:59 -0400

    sign up validation complete

--- react-app/src/components/auth/LoginForm.js
+++ react-app/src/components/auth/LoginForm.js
@@ -51,7 +51,7 @@ const LoginForm = () => {
         <label htmlFor='email'>Email</label>
         <input
           name='email'
-          type='text'
+          type='email'
           placeholder='Email'
           value={email}
           onChange={updateEmail}
