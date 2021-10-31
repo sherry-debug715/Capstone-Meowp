537c556a07924aa472f5619ea52a4338a93e2d51
sherry-debug715 <bambooyu715@gmail.com>
2021-10-31 16:11:45 -0400

    footer css complete

--- react-app/src/components/Footer/index.js
+++ react-app/src/components/Footer/index.js
@@ -11,24 +11,24 @@ const Footer = () => {
             <footer id="footer">
                 <div id="footer-upper">
                     <div className="slogan2">Designed and Engineered by Sherry Yu</div>
-                    <div className="read-about-me">
-                        <div>About Me</div>
-                            <div className="linkedin">
-                                <span>Linkedin: </span>
-                                <a className="link-linkedin" href="https://www.linkedin.com/in/xiaozhuyu/">
-                                    <img id="about-me-logo" src={linkedinLogo} alt="linkedin-logo"/>
-                                </a>
-                            </div>
-                            <div className="github">
-                                <span>GitHub: </span>
-                                <a className="link-gitHub" href="https://github.com/sherry-debug715">
-                                    <img id="about-me-logo" src={gitHubLogo} alt="gitHub-logo"/>
-                                </a>
-                            </div>
+                    <div className="read-about-me-container">
+                        <div className="read-about-me">
+                            <div className="professional-profile">Linkedin: </div>
+                            <a className="link-linkedin" href="https://www.linkedin.com/in/xiaozhuyu/">
+                                <img id="about-me-logo" src={linkedinLogo} alt="linkedin-logo"/>
+                            </a>
+                        </div>
+                        <div className="read-about-me">
+                            <div className="professional-profile">GitHub: </div>
+                            <a className="link-gitHub" href="https://github.com/sherry-debug715">
+                                <img id="about-me-logo" src={gitHubLogo} alt="gitHub-logo"/>
+                            </a>
+                        </div>
                     </div>
                 </div>
+
                 <div className="footer-bottom">
-                    <p>Copyright 2021 © All Rights Reserved</p>
+                    <p className="copyright">Copyright 2021 © All Rights Reserved</p>
                 </div>
             </footer>
     )
